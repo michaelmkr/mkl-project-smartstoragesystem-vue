94ef79f67363c6a0bdeee1974974cf0b39292099
MichaelMkr <michael.macher@humanizing.com>
2020-04-24 21:40:38 +0200

    started to implement material design into vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,88 @@
 <template>
-  <div id="app">
-    <headerbar></headerbar>
-    <menu-component></menu-component>
-    <main class="mdl-layout__content">
-      <router-view class="main-content"/>
-    </main>
-  </div>
+
+    <div class="page-container">
+        <md-app>
+            <md-app-toolbar>
+                <div id="header-logo"><img src="./assets/logo.svg"></div>
+                <span class="example-fill-remaining-space"></span>
+                <md-button @click="toggleMenu" class="md-icon-button" v-if="!menuVisible">
+                    <md-icon>menu</md-icon>
+                </md-button>
+            </md-app-toolbar>
+
+            <md-app-drawer :md-active.sync="menuVisible" :md-right="true" md-persistent="full">
+                <md-toolbar class="md-transparent" md-elevation="0">
+                    <span>Navigation</span>
+
+                    <div class="md-toolbar-section-end">
+                        <md-button @click="toggleMenu" class="md-icon-button md-dense">
+                            <md-icon>close</md-icon>
+                        </md-button>
+                    </div>
+                </md-toolbar>
+
+                <md-list>
+                    <md-list-item>
+                        <md-icon>move_to_inbox</md-icon>
+                        <router-link class="md-list-item-text" to="/">Home</router-link>
+                    </md-list-item>
+
+                    <md-list-item>
+                        <md-icon>move_to_inbox</md-icon>
+                        <router-link class="md-list-item-text" to="/store">Einlagern</router-link>
+                    </md-list-item>
+
+                    <md-list-item>
+                        <md-icon>delete</md-icon>
+                        <span class="md-list-item-text">Trash</span>
+                    </md-list-item>
+
+                    <md-list-item>
+                        <md-icon>error</md-icon>
+                        <span class="md-list-item-text">Spam</span>
+                    </md-list-item>
+                </md-list>
+            </md-app-drawer>
+
+            <md-app-content>
+                <!--main #app-->
+                <div id="app">
+                    <main class="mdl-layout__content">
+                        <router-view class="main-content"/>
+                    </main>
+                </div>
+            </md-app-content>
+        </md-app>
+    </div>
+
 </template>
 
 <script>
-import Headerbar from "@/components/headerbar";
-import MenuComponent from "@/components/MenuComponent";
-export default {
-  name: 'app',
-  components: {
-    MenuComponent,
-    Headerbar
-
-  }
-}
+    export default {
+        name: 'app',
+        components: {},
+        data: () => ({
+            menuVisible: false
+        }),
+        methods: {
+            toggleMenu() {
+                this.menuVisible = !this.menuVisible
+            }
+        },
+    }
 </script>
+
+<style scoped>
+    .md-app {
+        min-height: 100vh;
+    }
+
+    .md-drawer {
+        width: 230px;
+        max-width: calc(100vw - 125px);
+    }
+
+    .example-fill-remaining-space {
+        flex: 1 1 auto;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router';
+import store from "@/store";
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import VueMaterial from "vue-material";
+
 Vue.config.productionTip = false
 
+
+
+Vue.use(VueMaterial)
+
 new Vue({
-  router,
-  render: h => h(App),
+    store,
+    router,
+    render: h => h(App),
 }).$mount('#app')
