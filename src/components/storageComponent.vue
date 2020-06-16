b20c1ff8f857b0793edba3eb81621287876e9f83
MichaelMkr <michael.macher@humanizing.com>
2020-06-16 11:36:35 +0200

    updated the first try for a storage component

--- src/components/storageComponent.vue
+++ src/components/storageComponent.vue
@@ -1,6 +1,21 @@
 <template>
     <div>
-        <p>{{products}}</p>
+        <md-table>
+            <md-table-row>
+                <md-table-head>Frucht</md-table-head>
+                <md-table-head>Gewicht</md-table-head>
+                <md-table-head>Einlagerdatum</md-table-head>
+                <md-table-head>Anmerkungen</md-table-head>
+            </md-table-row>
+
+            <md-table-row v-for="product in products" :key="product.id">
+                <md-table-cell>{{product.fruitType}}</md-table-cell>
+                <md-table-cell>{{product.amount}} kg</md-table-cell>
+                <md-table-cell>{{product.storageDate}}</md-table-cell>
+                <md-table-cell>{{product.comments}}</md-table-cell>
+            </md-table-row>
+
+        </md-table>
     </div>
 </template>
 
@@ -10,7 +25,10 @@
     export default {
         name: "storageComponent",
         data() {
-            return {}
+            return {
+                currentSort: 'name',
+                currentSortOrder: 'asc',
+            }
         },
         mounted() {
             this.retrieveProductsCollection();
@@ -24,6 +42,17 @@
             ...mapActions([
                 'retrieveProductsCollection',
             ]),
+            customSort (value) {
+                return value.sort((a, b) => {
+                    const sortBy = this.currentSort
+
+                    if (this.currentSortOrder === 'desc') {
+                        return a[sortBy].localeCompare(b[sortBy])
+                    }
+
+                    return b[sortBy].localeCompare(a[sortBy])
+                })
+            },
             logSmthg() {
             },
         }
